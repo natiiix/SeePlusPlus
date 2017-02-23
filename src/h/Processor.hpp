91c8aebe40a75b4e52881766fb8000ba7844458c
Ivo Meixner <ivo.meixner@se7en.cz>
2017-02-23 01:12:38 +0100

    Major development, implemented basic instructions Added INC, DEC, ADD, ADC, SUB, SBB. MOV now supports binary and hexadecimal source operand.

--- src/h/Processor.hpp
+++ src/h/Processor.hpp
@@ -1,6 +1,11 @@
+#pragma once
+
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
+
+#include "misc.hpp"
 
 class Processor
 {
@@ -12,6 +17,8 @@ private:
   static const int REG_WIDTH = 8;
   signed int regInt[REG_WIDTH];
   unsigned long regLong[REG_WIDTH];
+  bool flagCarry;
+  bool flagBorrow;
 
   void errInvalidRegister(const std::string &strReg);
   void errInvalidOperand(const std::string &strOperand);
@@ -19,10 +26,20 @@ private:
   int getRegIndex(const std::string &strReg);
   bool getRegInt(const std::string &strReg, signed int *&pReg);
   bool getRegLong(const std::string &strReg, unsigned long *&pReg);
+  
+  void clearFlags();
 
   // Custom instructions
   bool instrDUMP(const std::string &strReg); // Prints the register to console
 
   // x86 instructions
-  bool instrMOV(const std::string &strDest, const std::string &strSource);
+  bool instrMOV(const std::string &strDest, const std::string &strSource); // Move
+  bool instrINC(const std::string &strDest); // Increment by 1
+  bool instrDEC(const std::string &strDest); // Decrement by 1
+  bool instrADD(const std::string &strDest, const std::string &strSource); // Add
+  bool instrADC(const std::string &strDest); // Add carry
+  bool instrADC(const std::string &strDest, const std::string &strSource); // Add with carry
+  bool instrSUB(const std::string &strDest, const std::string &strSource); // Subtract
+  bool instrSBB(const std::string &strDest); // Subtract borrow
+  bool instrSBB(const std::string &strDest, const std::string &strSource); // Subtract with borrow
 };
--- src/h/misc.hpp
+++ src/h/misc.hpp
@@ -3,5 +3,9 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 std::vector<std::string> split(const std::string &strInput, const char separator, const bool removeEmpty = false);
+bool strBeginsWith(const std::string & str, const std::string & beginning);
+bool strSIntTryParse(const std::string & strIn, signed int & iOut);
+bool strULongTryParse(const std::string & strIn, unsigned long & uOut);
